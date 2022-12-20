c62bcf2c98be6ae8257f7841c79f96eb2453cdf4
chhotu1 <chhotukumarsow@gmail.com>
2022-12-20 08:00:11 +0530

    resume

--- pages/resume.js
+++ pages/resume.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Data } from '../public/Data'
 const Resume = () => {
-  const {resumeData} = Data;
+  const { resumeData, aboutMe } = Data;
   return (
     <section id="resume" className="resume section-show">
       <div className="container">
@@ -13,19 +13,19 @@ const Resume = () => {
           <div className="col-lg-6">
             <h3 className="resume-title">Sumary</h3>
             <div className="resume-item pb-0">
-              <h4>Chhotu sow</h4>
-              <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
+              <h4>{aboutMe.name}</h4>
+              <p><em>{aboutMe.long_description}</em></p>
               <p>
               </p><ul>
-                <li>Khizersarai gaya,bihar India,824233</li>
-                <li>+917739422219</li>
-                <li>chhotukumaarsow@gmail.com</li>
+                <li>{aboutMe.address}</li>
+                <li>{aboutMe.phone}</li>
+                <li>{aboutMe.email}</li>
               </ul>
               <p />
             </div>
             <h3 className="resume-title">Education</h3>
-            {resumeData && resumeData.education.map((item,index)=>{
-              return(
+            {resumeData && resumeData.education.map((item, index) => {
+              return (
                 <div className="resume-item" key={index}>
                   <h4>{item.title}</h4>
                   <h5>{item.year}</h5>
@@ -37,24 +37,37 @@ const Resume = () => {
           </div>
           <div className="col-lg-6">
             <h3 className="resume-title">Professional Experience</h3>
-            {resumeData && resumeData.experience.map((item,index)=>{
-              return(
+            {resumeData && resumeData.experience.map((item, index) => {
+              return (
                 <div className="resume-item" key={index}>
                   <h4>{item.title}</h4>
                   <h5>{item.year}</h5>
                   <p><em>{item.address} </em></p>
                   <p>
                   </p><ul>
-                   {item.description.map((desc,i)=>{
-                    return(
-                      <li key={i}>{desc}</li>
-                    )
-                   })}
+                    {item.description.map((desc, i) => {
+                      return (
+                        <li key={i}>{desc}</li>
+                      )
+                    })}
                   </ul>
                   <p />
+                  {item.projects.map((project,_index)=>{
+                    return(
+                      <>
+                      <p>{_index+1}. <em> {project.link} </em></p>
+                      <p><ul>
+                        <li>{project.technology}</li>
+                        {/* <li>{project.description}</li> */}
+                        </ul></p>
+                      
+                      </>
+                    )
+                  })}
                 </div>
-                )})}
-            
+              )
+            })}
+
           </div>
         </div>
       </div>
--- public/Data.js
+++ public/Data.js
@@ -1,4 +1,17 @@
 export const Data = {
+    aboutMe: {
+        name: "Chhotu sow",
+        dob: "14 August 1996",
+        website: "chhotu-portfolio.vercel.app",
+        phone: "+918210118348",
+        age: 30,
+        degree: "Master in computer application",
+        freelance: "Available",
+        email: "chhotukumarsow@gmail.com",
+        address: "Khizersarai gaya,Bihar India,824233",
+        long_description: "A seasoned profession in Software Development. Experience React developer and highly skilled in brainstorming and coding features and enhancements. To contribute my best efforts to the organization with hard work and dedication for the development of the organization.Possesses an understanding developing end-to-end application as per client requirements and quality norms. Adept at conducting automation testing and  performing regular audits to fix bugs & improve work efficiency",
+        short_description: "I am software engineer"
+    },
     aboutData: {
         skill: [
             { id: 1, title: "HTML", rank: 30 },
@@ -27,18 +40,32 @@ export const Data = {
             { id: 4, title: "Matriculation ", year: "2011-2012", college: "Yashwant High School, Khizersarai - Gaya, Bihar, India", description: "" },
         ],
         experience: [
-            {id:1,title:"SENIOR GRAPHIC DESIGN SPECIALIST",company_name:"Experion, New York, NY",year:"2020-Present",address:"Stepping Stone Advertising, New York, NY",description:[
-                "Lead in the design, development, and implementation of the graphic, layout, and production communication materials",
-                "Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project.",
-                "Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design",
-                "Oversee the efficient use of production project budgets ranging from $2,000 - $25,000"
-            ]},
-            {id:2,title:"GRAPHIC DESIGN SPECIALIST",company_name:"Experion, New York, NY",year:"2021-2022",address:"Stepping Stone Advertising, New York, NY",description:[
-                "Lead in the design, development, and implementation of the graphic, layout, and production communication materials",
-                "Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project.",
-                "Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design",
-                "Oversee the efficient use of production project budgets ranging from $2,000 - $25,000"
-            ]}
+            {
+                id: 1, title: "MEAN & MERN STACK DEVELOPER", company_name: "PixlerLab", year: "2021-Present", address: "E-237AA, Continental Tower, Industrial Area, Sector 74, Sahibzada Ajit Singh Nagar, Punjab 140308", description: [
+                    "Good Problem solving and analytical skill",
+                    "Leveraged MVC design pattern to organize Angular controllers, Custom directives, factories and views",
+                    "Worked on the restful API to create the services and test on postman and used in angular ,react js and next js call and bind the data",
+                    "Leveraged MVC design pattern to organize Angular controllers, Custom directives, factories and views"
+                ],
+                projects: [
+                    { link: "https://www.pixlerlab.com/", technology: "Nextjs,Nodejs,Mongodb", description: "" },
+                    { link: "https://hadsup.com/", technology: "Nextjs", description: "" }, 
+                    { link: "https://app.hadsup.com/", technology: "Angular", description: "" }, 
+                    { link: "https://1099prep.com/", technology: "Reactjs,MYsql,Laravel", description: "" },
+                ],
+            },
+            {
+                id: 2, title: "ANGULAR DEVELOPER", company_name: "WeaveBytes Infotech Pvt Ltd", year: "2019-2020", address: "Software company in Kharar, SAS Nagar, Punjab", description: [
+                    "Working knowledge of front end optimization and performance techniques",
+                    "Cooperating with the back-end developer in the process of building the restful API",
+                    "worked on the restful API to create the services and test on postman and used in angular http service call and bind the data",
+                    "Built web pages that are more user-interactive using jQuery AJAX, JSON, Angular JS, and JavaScript, Bootstrap",
+                    "Leveraged MVC design pattern to organize Angular controllers, Custom directives, factories and views"
+                ],
+                projects: [
+                    { link: "http://control.trackugo.in/", technology: "Laravel,Mongodb", description: "" },
+                ],
+            }
         ],
     }
 }
